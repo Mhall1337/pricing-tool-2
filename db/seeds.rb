743c4f32e8c17cabc3ebc0a547687cf62b3bddd9
Mitchell <Hmitchell.hall@gmail.com>
2022-03-16 10:58:48 -0500

    added a little seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Carrier.create(carrier_name: 'Landstar')
+Carrier.create(carrier_name: "PS Logistics")
+Carrier.create(carrier_name: "MTB")
+
+
+Dispatcher.create(dispatcher_name: 'bill', dispatcher_phone_number: 18008111, email:'bill@aol.com', carrier_id: 1)
+Dispatcher.create(dispatcher_name: 'jan', dispatcher_phone_number: 9834795, email:'jan@aol.com', carrier_id: 2)
+Dispatcher.create(dispatcher_name: 'carol', dispatcher_phone_number: 98734905, email:'carol@aol.com', carrier_id: 2)
+Dispatcher.create(dispatcher_name: 'chip', dispatcher_phone_number: 235364, email:'chip@mtb.com', carrier_id: 3)
+Dispatcher.create(dispatcher_name: 'pete', dispatcher_phone_number: 6234623, email:'pete@mtb.com', carrier_id: 3)
+
+Shipment.create(origin_city: 'chicago', origin_state: 'IL', destination_city: 'houston', destination_state: 'TX', commodity:'steel', trailer_type:'FB', carrier_id:1, miles: 750, rate: 3000, driver_name:'billy bob', driver_phone_number: 489287 )
