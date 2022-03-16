546b10e6d0676f903605cf420b78726ccd441eb2
Mitchell <Hmitchell.hall@gmail.com>
2022-03-16 11:08:13 -0500

    added faker gem and a bit more fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,15 @@ Dispatcher.create(dispatcher_name: 'chip', dispatcher_phone_number: 235364, emai
 Dispatcher.create(dispatcher_name: 'pete', dispatcher_phone_number: 6234623, email:'pete@mtb.com', carrier_id: 3)
 
 Shipment.create(origin_city: 'chicago', origin_state: 'IL', destination_city: 'houston', destination_state: 'TX', commodity:'steel', trailer_type:'FB', carrier_id:1, miles: 750, rate: 3000, driver_name:'billy bob', driver_phone_number: 489287 )
+
+50.times do
+    carrier = Carrier.create(carrier_name: Faker::Company.name)
+    5.times do
+      carrier.dispatchers.create(dispatcher_name: Faker::Name.name, dispatcher_phone_number: Faker::PhoneNumber.phone_number, email: Faker::Internet.email, carrier_id: carrier.id)
+    end
+end
+
+
+  10.times do
+    ship =Shipment.create(origin_city: Faker::Address.city, origin_state: Faker::Address.state_abbr, destination_city: Faker::Address.city, destination_state: Faker::Address.state_abbr, commodity: Faker::Commerce.material, trailer_type: "FB", carrier_id: rand(1..50), miles: rand(1..1000), rate: rand(1..4000), driver_name: Faker::Name.name, driver_phone_number: Faker::PhoneNumber.phone_number)
+  end
