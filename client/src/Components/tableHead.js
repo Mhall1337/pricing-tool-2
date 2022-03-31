74bb1c478c381b2cfa209cf5f7b081109b384389
Mitchell <Hmitchell.hall@gmail.com>
2022-03-31 14:20:13 -0500

    added first half of sort to sortable columns

--- client/src/Components/tableHead.js
+++ client/src/Components/tableHead.js
@@ -1,10 +1,31 @@
-function TableHead({setShipments, shipments}) {
-
+function TableHead({ setShipments, shipments }) {
     function sort(e) {
-        console.log(e.target.textContent)
+        const columnName = e.target.textContent
         const sortedShipments = shipments.sort(function (a, b) {
-            const nameA = a.carrier.carrier_name.toUpperCase()
-            const nameB = b.carrier.carrier_name.toUpperCase()
+            let nameA
+            let nameB
+            switch (columnName) {
+                case "Origin City":
+                    nameA = a.origin.city.toUpperCase()
+                    nameB = b.origin.city.toUpperCase()
+                    break;
+                case "Origin State":
+                    nameA = a.origin.state_abbr.toUpperCase()
+                    nameB = b.origin.state_abbr.toUpperCase()
+                    break;
+                case "Carrier Name":
+                    nameA = a.carrier.carrier_name.toUpperCase()
+                    nameB = b.carrier.carrier_name.toUpperCase()
+                    break;
+                case "Dest. City":
+                    nameA = a.destination.city.toUpperCase()
+                    nameB = b.destination.city.toUpperCase()
+                    break;
+                case "Dest. State":
+                    nameA = a.destination.state_abbr.toUpperCase()
+                    nameB = b.destination.state_abbr.toUpperCase()
+                    break;
+            }
             if (nameA < nameB) {
                 return -1;
             }
@@ -16,23 +37,23 @@ function TableHead({setShipments, shipments}) {
         setShipments([...sortedShipments])
     }
 
-        return (
-            <thead>
-                <tr>
-                    <th className="grid-item" onClick={sort}>Origin City</th>
-                    <th className="grid-item" onClick={sort}>Origin state</th>
-                    <th className="grid-item">Commodity</th>
-                    <th className="grid-item" onClick={sort}>Carrier Name</th>
-                    <th className="grid-item" onClick={sort}>Dest. City</th>
-                    <th className="grid-item" onClick={sort}>Dest. state</th>
-                    <th className="grid-item">Miles</th>
-                    <th className="grid-item">Rate</th>
-                    <th className="grid-item">Dispatcher Name</th>
-                    <th className="grid-item">Dispatcher Phone Number</th>
-                    <th className="grid-item">Dispatcher Email Address</th>
-                </tr>
-            </thead>
-        )
-    
+    return (
+        <thead>
+            <tr>
+                <th className="grid-item" onClick={sort}>Origin City</th>
+                <th className="grid-item" onClick={sort}>Origin State</th>
+                <th className="grid-item">Commodity</th>
+                <th className="grid-item" onClick={sort}>Carrier Name</th>
+                <th className="grid-item" onClick={sort}>Dest. City</th>
+                <th className="grid-item" onClick={sort}>Dest. State</th>
+                <th className="grid-item">Miles</th>
+                <th className="grid-item">Rate</th>
+                <th className="grid-item">Dispatcher Name</th>
+                <th className="grid-item">Dispatcher Phone Number</th>
+                <th className="grid-item">Dispatcher Email Address</th>
+            </tr>
+        </thead>
+    )
+
 }
-    export default TableHead
+export default TableHead
