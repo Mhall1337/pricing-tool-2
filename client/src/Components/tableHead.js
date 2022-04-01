e268b086d9318cf532c26fd66ed3c70e945165c6
Mitchell <Hmitchell.hall@gmail.com>
2022-04-01 10:04:26 -0500

    reversed order of column sorting from descending first to ascending first

--- client/src/Components/tableHead.js
+++ client/src/Components/tableHead.js
@@ -4,25 +4,25 @@ function TableHead({ setShipments, shipments }) {
 
 const [sortOrder, setSortOrder] = useState(true)
 
-    function sort(e) {
+    function sortColumn(e) {
         setSortOrder(!sortOrder)
         let order
         const columnName = e.target.textContent
             switch (columnName) {
                 case "Origin City":
-                    sortOrder ? order = shipments.sort((a, b) => b.origin.city.localeCompare(a.origin.city)) : order = shipments.sort((a, b) => a.origin.city.localeCompare(b.origin.city))
+                    sortOrder ? order = shipments.sort((a, b) => a.origin.city.localeCompare(b.origin.city)) : order = shipments.sort((a, b) => b.origin.city.localeCompare(a.origin.city))
                     break;
                  case "Origin State":
-                   sortOrder ? order = shipments.sort((a, b) => b.origin.state_abbr.localeCompare(a.origin.state_abbr)) : order = shipments.sort((a, b) => a.origin.state_abbr.localeCompare(b.origin.state_abbr))
+                   sortOrder ? order = shipments.sort((a, b) => a.origin.state_abbr.localeCompare(b.origin.state_abbr)) : order = shipments.sort((a, b) => b.origin.state_abbr.localeCompare(a.origin.state_abbr))
                     break;
                 case "Carrier Name":
-                    sortOrder ? order = shipments.sort((a, b) => b.carrier.carrier_name.localeCompare(a.carrier.carrier_name)) : order = shipments.sort((a, b) => a.carrier.carrier_name.localeCompare(b.carrier.carrier_name))
+                    sortOrder ? order = shipments.sort((a, b) => a.carrier.carrier_name.localeCompare(b.carrier.carrier_name)) : order = shipments.sort((a, b) => b.carrier.carrier_name.localeCompare(a.carrier.carrier_name))
                     break;
                 case "Dest. City":
-                    sortOrder ? order = shipments.sort((a, b) => b.destination.city.localeCompare(a.destination.city)) : order = shipments.sort((a, b) => a.destination.city.localeCompare(b.destination.city))
+                    sortOrder ? order = shipments.sort((a, b) => a.destination.city.localeCompare(b.destination.city)) : order = shipments.sort((a, b) => b.destination.city.localeCompare(a.destination.city))
                     break;
                 case "Dest. State":
-                    sortOrder ? order = shipments.sort((a, b) => b.destination.state_abbr.localeCompare(a.destination.state_abbr)) : order = shipments.sort((a, b) => a.destination.state_abbr.localeCompare(b.destination.state_abbr))
+                    sortOrder ? order = shipments.sort((a, b) => a.destination.state_abbr.localeCompare(b.destination.state_abbr)) : order = shipments.sort((a, b) => b.destination.state_abbr.localeCompare(a.destination.state_abbr))
                     break;
             }
             
@@ -33,12 +33,12 @@ const [sortOrder, setSortOrder] = useState(true)
     return (
         <thead>
             <tr>
-                <th className="grid-item" onClick={sort}>Origin City</th>
-                <th className="grid-item" onClick={sort}>Origin State</th>
+                <th className="grid-item" onClick={sortColumn}>Origin City</th>
+                <th className="grid-item" onClick={sortColumn}>Origin State</th>
                 <th className="grid-item">Commodity</th>
-                <th className="grid-item" onClick={sort}>Carrier Name</th>
-                <th className="grid-item" onClick={sort}>Dest. City</th>
-                <th className="grid-item" onClick={sort}>Dest. State</th>
+                <th className="grid-item" onClick={sortColumn}>Carrier Name</th>
+                <th className="grid-item" onClick={sortColumn}>Dest. City</th>
+                <th className="grid-item" onClick={sortColumn}>Dest. State</th>
                 <th className="grid-item">Miles</th>
                 <th className="grid-item">Rate</th>
                 <th className="grid-item">Dispatcher Name</th>
