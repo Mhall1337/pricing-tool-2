4e8a3af66ad2ac50d298965c5e188c5bb4a3a4b9
Mitchell <Hmitchell.hall@gmail.com>
2022-04-06 12:16:35 -0500

    completed form to update state with input values and creating fetch request. need custom route

--- client/src/Components/searchBar.js
+++ client/src/Components/searchBar.js
@@ -2,33 +2,57 @@ import { useState } from "react"
 
 function SearchBar() {
     const [formVal, setFormVal] = useState({
-        origin_city: '',
-        origin_state: '',
-        carrier: '',
-        destination_city: '',
-        destination_state: ''
+        origin_city: '*',
+        origin_state: '*',
+        carrier: '*',
+        destination_city: '*',
+        destination_state: '*'
     })
-    function handleSearch(e){
-        console.log(e.target.values)
+    console.log(formVal)
+    function handleSearch(e) {
+        e.preventDefault()
+        setFormVal({
+            origin_city: e.target[0].value,
+            origin_state: e.target[1].value,
+            carrier: e.target[2].value,
+            destination_city: e.target[3].value,
+            destination_state: e.target[4].value
+        })
+        e.target.reset()
+        fetch('/searchShipments', {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(formVal)
+        })
+            .then(r => r.json())
+            .then(r => {
+                if (r.ok) {
+                    console.log(r)
+                }
+            })
     }
-    
+
+
+
     return (
         <div>
             <hr></hr>
             <div>Search</div>
-            <form>
+            <form onSubmit={handleSearch} >
                 <label>Origin City: </label>
-                <input placeholder="Search Origin City" type='text'></input>
+                <input placeholder="Search Origin City" type='text' name="origin_city"></input>
                 <label> Origin State: </label>
-                <input placeholder="Search Origin State" type='text'></input>
+                <input placeholder="Search Origin State" type='text' name="origin_state" ></input>
                 <label> Destination City: </label>
-                <input placeholder="Search Destination City" type='text'></input>
+                <input placeholder="Search Destination City" type='text' name="destination_city" ></input>
                 <label> Destination State: </label>
-                <input placeholder="Search Destination State" type='text'></input>
+                <input placeholder="Search Destination State" type='text' name="destination_state" ></input>
                 <label> Carrier: </label>
-                <input placeholder="Search Carrier"></input>
-                <input type="submit"></input>
-            </form>
+                <input placeholder="Search Carrier" name="carrier" ></input>
+                <input type="submit" value='submit'></input>
+            </form >
             <hr></hr>
         </div>
 
