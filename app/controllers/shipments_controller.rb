ef53d918c38b296cec499909161959225e66ce71
Mitchell <Hmitchell.hall@gmail.com>
2022-04-07 16:45:37 -0500

    added method in shipment model to find duplicate shipments

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,20 +1,15 @@
 class ShipmentsController < ApplicationController
-    before_action :force_json, only: :searc
-    def index
-        shipments = Shipment.all
-        render json: shipments, include: [:dispatcher, :carrier, :origin, :destination]
-    end
-    def search
+  def index
+    shipments = Shipment.all
+    render json: shipments, include: [:dispatcher, :carrier, :origin, :destination]
+  end
 
-        render json: Location.all
-    end
+  def search
+  end
 
+  private
 
-    private
-    def permitParams
-        params.permit(:origin_city, :origin_state, :destination_city, :destination_state, :carrier)
-    end
-    def force_json
-        request.format = :json
-      end
+  def permitParams
+    params.permit(:origin_city, :origin_state, :destination_city, :destination_state, :carrier)
+  end
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -3,8 +3,39 @@ class Shipment < ApplicationRecord
     belongs_to :dispatcher
     belongs_to :origin, class_name: 'Location'
     belongs_to :destination, class_name: 'Location'
+    #@@uni_arr
+    @non_uni
+    
 
-    def self.searchedShipments()
-        self.all.select{|e| }
+    # def self.unique_shipments
+    #    unique = self.all.map{|e| [e.origin.city, e.origin.state_abbr, e.carrier, e.destination.city, e.destination.state_abbr]}
+    #    self.all.map.with_index{|shipment, i| shipment if [shipment.origin.city, shipment.origin.state_abbr, shipment.carrier, shipment.destination.city, shipment.destination.state_abbr] == unique[i]}
+    # end
+
+    # def self.uni_two
+    #     #@@uni_arr = []
+    #     # @@non_uni =[]
+    #     unique = self.all.map{|e| [e.origin.city, e.origin.state_abbr, e.carrier, e.destination.city, e.destination.state_abbr]}.uniq
+    #     self.all.each.with_index do |shipment, i|
+    #         if [shipment.origin.city, shipment.origin.state_abbr, shipment.carrier, shipment.destination.city, shipment.destination.state_abbr] == unique[i]
+    #           #  @@uni_arr << shipment
+    #         else
+    #             #@@non_uni << shipment
+    #             i+=1
+    #         end
+    #     end
+    # end
+
+    def find_duplicates 
+        shipments = Shipment.all.map{|e| [e.origin.city, e.origin.state_abbr, e.carrier, e.destination.city, e.destination.state_abbr]}
+        @non_uni = shipments.select.with_index do |e, i|
+            i != shipments.index(e)
+        end
+    end
+    
+    def handle_duplicate
+        Shipment.all.select do |e|
+            [e.origin.city, e.origin.state_abbr, e.carrier, e.destination.city, e.destination.state_abbr] == @non_uni
+        end
     end
 end
