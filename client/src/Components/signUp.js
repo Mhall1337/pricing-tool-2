e00c4c5a933db3d82f22bb5c1ecd320f73a19c5d
Mitchell <Hmitchell.hall@gmail.com>
2022-04-14 14:05:41 -0500

    added style sheet for signup form. still needs a little work

--- client/src/Components/signUp.js
+++ client/src/Components/signUp.js
@@ -1,33 +1,36 @@
 import { useState } from "react"
+import './signup.css'
 
 function SignUp() {
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
 
-  function handleSubmit(e){
-      e.preventDefault()
-      fetch('http://localhost:3000/signup',{
-          method: 'POST',
-          headers:{
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({username: userName, password: password, confirmPassword: confirmPassword})
-      })
-      .then(r => r.json())
-      .then(r => console.log(r))
-      .catch(error => console.log(error))
-  }  
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('clicked')
+        //   fetch('http://localhost:3000/signup',{
+        //       method: 'POST',
+        //       headers:{
+        //           'Content-Type': 'application/json'
+        //       },
+        //       body: JSON.stringify({username: userName, password: password, confirmPassword: confirmPassword})
+        //   })
+        //   .then(r => r.json())
+        //   .then(r => console.log(r))
+        //   .catch(error => console.log(error))
+    }
 
     return (
         <div>
-            <h1>SIGN UP</h1>
-            <form onSubmit={handleSubmit}>
+
+            <form onSubmit={handleSubmit} className="signupForm">
+                <h1>SIGN UP</h1>
                 <label>Username:  </label>
                 <input type="text" placeholder="username" value={userName} onChange={e => setUserName(e.target.value)}></input>
                 <br></br>
                 <label>Password:  </label>
-                <input type="text" placeholder="Password"value={password} onChange={e => setPassword(e.target.value)}></input>
+                <input type="text" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}></input>
                 <br></br>
                 <label>Confirm Password:  </label>
                 <input type="text" placeholder="Confirm Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}></input>
