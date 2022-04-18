3363bf751b64b0d5bac050501a0471d14f684794
Mitchell <Hmitchell.hall@gmail.com>
2022-04-18 16:41:43 -0500

    longitude and latitude for locations is mapped by geocode gem on initialization

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,11 @@
 class Location < ApplicationRecord
     has_many :shipments
-    # geocoded_by :address
-    # after_validation :geocode
+    geocoded_by :address
+    after_validation :geocode
 
     def address
         [self.city, self.state_abbr].compact.join(', ')
     end
+
+    
 end
