06433234058a8386904c4e5931e6b46bfb71e505
Mitchell <Hmitchell.hall@gmail.com>
2022-04-26 09:55:53 -0500

    added some custome styling to the table

--- client/src/Components/shipments.js
+++ client/src/Components/shipments.js
@@ -53,18 +53,18 @@ function Shipments() {
                     {shipments.map((shipment, index) => {
                         const { commodity, carrier, dispatcher, miles, rate, destination, origin } = shipment
                         return (
-                            <tr key={index}>
-                                <td>{origin.city}</td>
-                                <td>{origin.state_abbr}</td>
-                                <td>{commodity}</td>
-                                <td>{carrier.carrier_name}</td>
-                                <td>{destination.city}</td>
-                                <td>{destination.state_abbr}</td>
-                                <td>{miles}</td>
-                                <td>{rate}</td>
-                                <td>{dispatcher.dispatcher_name}</td>
-                                <td>{dispatcher.dispatcher_phone_number}</td>
-                                <td>{dispatcher.email}</td>
+                            <tr className="table-row" key={index}>
+                                <td className="table-data">{origin.city}</td>
+                                <td className="table-data">{origin.state_abbr}</td>
+                                <td className="table-data">{commodity}</td>
+                                <td className="table-data">{carrier.carrier_name}</td>
+                                <td className="table-data">{destination.city}</td>
+                                <td className="table-data">{destination.state_abbr}</td>
+                                <td className="table-data">{miles}</td>
+                                <td className="table-data">{rate}</td>
+                                <td className="table-data">{dispatcher.dispatcher_name}</td>
+                                <td className="table-data">{dispatcher.dispatcher_phone_number}</td>
+                                <td className="table-data">{dispatcher.email}</td>
                             </tr>
                         )
                     })}
