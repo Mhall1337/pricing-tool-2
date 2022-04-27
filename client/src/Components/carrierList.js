87416697599f70370b4c8c459201e0b872bb6c09
Mitchell <Hmitchell.hall@gmail.com>
2022-04-27 14:57:04 -0500

    added fetch to carrierList component

--- client/src/Components/carrierList.js
+++ client/src/Components/carrierList.js
@@ -1,5 +1,18 @@
+import { useState } from "react"
+
 export default function CarrierList(){
+
+    const [notes, setNotes] = useState([])
+
+    function getCarrierNotes(){
+        fetch('http://localhost:3000/carrier_notes')
+        .then(r => r.json())
+        .then(r => console.log(r))
+    }
+
     return(
-        <div className="carrier-list-container">Carrier List</div>
+        <div className="carrier-list-container">Carrier List
+        <button onClick={getCarrierNotes}>get</button>
+        </div>
     )
 }
