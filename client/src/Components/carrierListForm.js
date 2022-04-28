e81d5dcbdc7e95377052b941b596c54469293781
Mitchell <Hmitchell.hall@gmail.com>
2022-04-28 10:21:54 -0500

    added state to carrierListForm component

--- client/src/Components/carrierListForm.js
+++ client/src/Components/carrierListForm.js
@@ -4,14 +4,17 @@ export default function CarrierListForm(){
 
     const [carrier, setCarrier]=useState('')
     const [note, setNote]=useState('')
+    console.log(carrier)
+    console.log(note)
 
     return(
         <div>
             <form>
                 <label>Carrier Name: </label>
-                <input type="text" placeholder="Carrier Name"></input>
+                <input type="text" placeholder="Carrier Name" value={carrier} onChange={e=>setCarrier(e.target.value)}></input>
                 <label>Notes</label>
-                <textarea value={note}>needs a value attribute</textarea>
+                <textarea value={note} onChange={e=>setNote(e.target.value)}>needs a value attribute</textarea>
+                <input type="submit"></input>
             </form>
         </div>
     )
