36a7103dbd74131cecf80c41c1fb46549cd8c3c8
Mitchell <Hmitchell.hall@gmail.com>
2022-05-16 10:55:45 -0500

    fixed another state issue where the carrier dropdown wasnt updating after change

--- client/src/Components/carrierListForm.js
+++ client/src/Components/carrierListForm.js
@@ -8,23 +8,27 @@ export default function CarrierListForm({postNote}) {
     const [select, setSelect] = useState([])
     const [carriers, setCarriers] = useState([])
 
+  
     useEffect(() =>
         fetch("/carriers")
             .then(r => r.json())
-            .then(r => setCarriers(r))
+            .then(r => {
+                setCarriers(r)
+                setSelect(r)
+            })
         , [])
 
-        //update the options for carriers dropdown
-    function changeSelect() {
-        const result = carriers.filter(carrier => carrier.carrier_name.toLowerCase().includes(inputCarrier.toLowerCase()))
+        //update options for carriers dropdown
+    function changeSelect(e) {
+        const result = carriers.filter(carrier => carrier.carrier_name.toLowerCase().includes(e.target.value.toLowerCase()))
         setSelect(result)
     }
-
+    
  
     return (
         <div className="search-bar" id="carrier-search">
             <hr></hr>
-            <form onChange={changeSelect} onSubmit={(e)=>postNote(e, note)} className="carrier-list-form">
+            <form onChange={(e)=>changeSelect(e)} onSubmit={(e)=>postNote(e, note)} className="carrier-list-form">
                 <div>Carrier Name:</div>
                 <input type="text" placeholder="Carrier Name" value={inputCarrier} onChange={e => setInputCarrier(e.target.value)}></input>
                 <select>
